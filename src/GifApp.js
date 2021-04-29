d48c701299f996bab46e65a4149e558518957cbc
CEIBA\fernan.roman <fernan.roman@ceiba.com.co>
2021-04-28 21:43:08 -0500

    Logic is created to verify if new category existe, it is true the array is reordered to show the category in the first position, it is not only add the new category to the array

--- src/GifApp.js
+++ src/GifApp.js
@@ -6,11 +6,23 @@ import { Container } from 'react-bootstrap';
 const GifApp = () => {
   const [categories, setCategories] = useState(['Kimetsu no Yaiba']);
   const handleAdd = (newCategory) => {
-    const categoryFound = categories.find(category => category === newCategory);
-    if(!categoryFound) {
+    const startPosition = 0;
+    const isTheSameCategory = (category) => category === newCategory;
+    const categoryFound = categories.find(isTheSameCategory);
+    if (categoryFound) {
+      const indexFoundCategory = categories.findIndex(isTheSameCategory);
+      moveInArray(categories, indexFoundCategory, startPosition);
+      setCategories([...categories]);
+    } else {
       setCategories([newCategory, ...categories]);
     }
   };
+
+  const moveInArray = function (array, from, to) {
+    const item = array.splice(from, 1);
+    array.splice(to, 0, item[0]);
+  };
+
   return (
     <>
       <Container>
